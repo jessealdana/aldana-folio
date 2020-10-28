09ec8d96913fd0bf8b7b428a3844616437338edf
Jesse Aldana <jesse.aldana@gmail.com>
2020-10-28 12:55:46 -0700

    added jumbotron

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,8 +10,8 @@ function Home() {
       <Container>
         <div className="container">
             <Jumbotron>
-              <div className="centerpiece" style={{textAlign: "center", width: "47vw"}}>
-                <img src={aboutBackground} className="img-fluid" alt="McWay Falls" style={{height: "70vh"}}/> 
+              <div className="jumbotron" style={{textAlign: "center", width: "47vw", marginTop: "10px"}}>
+                <img src={aboutBackground} className="img-fluid" alt="McWay Falls" style={{height: "60vh"}}/> 
                 <h1 className="Subject" style={{position: "relative", backgroundColor: "whitesmoke", paddingLeft: "5px"}}>Jesse Aldana</h1>
                 <p className="Title" style={{position: "relative", color: "white", backgroundColor: "rgb(148, 151, 151)", paddingLeft: "5px"}}>Full-Stack Software Developer</p>
                 </div>
