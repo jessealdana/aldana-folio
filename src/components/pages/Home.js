580c01b06fb62daebf365c8f6e4291f2b4298638
Jesse Aldana <jesse.aldana@gmail.com>
2020-10-28 09:27:38 -0700

    resized homepage photo

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,11 +8,11 @@ function Home() {
   return (
     <Container>
       <div className="container">
-          <Jumbotron >
+          <Jumbotron>
 
-              <img src={aboutBackground} className="img-fluid" alt="McWay Falls" /> 
-              <h1 className="Subject" style={{backgroundColor: "whitesmoke", paddingLeft: "5px"}}>Jesse Aldana</h1>
-              <p className="Title" style={{color: "white", backgroundColor: "rgb(148, 151, 151)", paddingLeft: "5px"}}>Full-Stack Software Developer</p>
+              <img src={aboutBackground} className="img-fluid" alt="McWay Falls" style={{height: "70vh", width: "60vw"}}/> 
+              <h1 className="Subject" style={{backgroundColor: "whitesmoke", paddingLeft: "5px", width: "vw"}}>Jesse Aldana</h1>
+              <p className="Title" style={{color: "white", backgroundColor: "rgb(148, 151, 151)", paddingLeft: "5px", width: "vw"}}>Full-Stack Software Developer</p>
 
           </Jumbotron>
       </div>
